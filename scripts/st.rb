f5098215f04d90c6496ca774745a2ea083142b58
Sherman Rofeman <skippyr.developer@gmail.com>
2023-07-25 18:30:22 -0300

    ~% Refactor St theme.

---
+++ scripts/st.rb
@@ -0,0 +1,13 @@
+require_relative("libraries/utilities")
+
+Write_Theme_File(
+    "static const char *colorname[16] = {" +
+    Create_Colors_String(lambda {
+        |ansi, color_hex, __color_name| "#{
+            ansi % 7 == 0 ? "\n\t" : ", "
+        }#{color_hex}"
+    }) +
+    "\n};
+unsigned int defaultfg = 15, defaultbg = 0, defaultcs = 15;
+static unsigned int defaultrcs = 0;"
+)

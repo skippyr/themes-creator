c2052f4a29a83c2991dc2f77454558a2da8ceedf
Sherman Rofeman <skippyr.developer@gmail.com>
2023-07-25 18:48:06 -0300

    ~% Fix wrong placement of commas in St script.

--- scripts/st.rb
+++ scripts/st.rb
@@ -4,10 +4,10 @@ Write_Theme_File(
     "static const char *colorname[16] = {" +
     Create_Colors_String(lambda {
         |ansi, color_hex, __color_name| "#{
-            ansi % 7 == 0 ? "\n\t" : ", "
-        }#{Quote(color_hex)}"
+            ansi % 7 == 0 ? "\n\t" : " "
+        }#{Quote(color_hex)},"
     }) +
     "\n};
 unsigned int defaultfg = 15, defaultbg = 0, defaultcs = 15;
-static unsigned int defaultrcs = 0;"
-)
+static unsigned int defaultrcs = 0;
+")

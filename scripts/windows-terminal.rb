aa0f9d4d303dcf2cc842d5e044ea81a077e12254
Sherman Rofeman <skippyr.developer@gmail.com>
2023-07-26 12:18:38 -0300

    ~% Correct indentation for Windows terminal script.

--- scripts/windows-terminal.rb
+++ scripts/windows-terminal.rb
@@ -8,12 +8,14 @@ Write_Theme_File("/*
 {
     #{Quote("name")}: #{Quote($metadata[:name])},
 " + Create_Colors_String(lambda {
-        |ansi, color_hex, color_name|
-        "    #{Quote(color_name)}: #{Quote(color_hex)},\n"
-    }, true, false, true) +Create_Colors_String(lambda {
-        |ansi, color_hex, color_name|
-        "    #{Quote("bright" + color_name[0].upcase() + color_name[1..])}: #{Quote(color_hex)},\n"
-    }, true, true, true) +
+    |ansi, color_hex, color_name|
+    "    #{Quote(color_name)}: #{Quote(color_hex)},\n"
+}, true, false, true) + Create_Colors_String(lambda {
+    |ansi, color_hex, color_name|
+    "    #{Quote(
+        "bright" + color_name[0].upcase() + color_name[1..]
+    )}: #{Quote(color_hex)},\n"
+}, true, true, true) +
 "    #{Quote("background")}: #{Quote($metadata[:colors_hex][0])},
     #{Quote("foreground")}: #{Quote($metadata[:colors_hex][7])},
     #{Quote("cursorColor")}: #{Quote($metadata[:colors_hex][7])},

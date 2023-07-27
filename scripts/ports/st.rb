ad14a869801c03bf2db01b46b32f4a5b25c83488
Sherman Rofeman <skippyr.developer@gmail.com>
2023-07-27 19:41:07 -0300

    ~% Add missing colon to St theme file

--- scripts/ports/st.rb
+++ scripts/ports/st.rb
@@ -5,6 +5,6 @@ write_theme_file("static const char *colorname[16] = {
   |_color_ansi, color_hex, _color_name|
   "\t\"#{color_hex}\","
 }).join("\n")}
-}
+};
 unsigned int defaultfg = 15, defaultbg = 0, defaultcs = 15;
 static unsigned int defaultrcs = 0;\n")

1b20d97cf5b735b3870cab78ecce64866c4a3b13
Sherman Rofeman <skippyr.developer@gmail.com>
2023-08-05 12:20:28 -0300

    ~% update code style

--- README.md
+++ README.md
@@ -1,33 +1,26 @@
 # Themes Creator
-
 ## About
-
 A script that creates theme files based on templates.
 ![](preview.png)
 
 ## Installation
-
 ### Dependencies
-
--   ruby
--   git
+-	ruby
+-	git
 
 ### Procedures
-
 -   Clone this repository.
 
 ```bash
 git clone --depth=1 https://github.com/skippyr/themes-creator
 ```
 
--   Access the repository's directory.
-
+-	Access the repository's directory.
 ```bash
 cd themes-creator
 ```
 
 ## Usage
-
 Use the following table as a reference:
 | Line Number | Description | Replaces |
 |-|-|-|
@@ -47,36 +40,21 @@ Use the following table as a reference:
 Colors use ANSI convention as reference. They must use HEX format, starting with the `#` character and without opacity attributes. Their case does not matter.
 
 ### Creating A Theme From Template
+-	Create a metadata file for your theme using the `metadata.template` file as a template or as an example to use with the script. The lines inside of that file follows the convention used in the reference table for the `Line Number` and `Description` columns.
 
--   Create a metadata file for your theme using the `metadata.template` file as
-    a template or as an example to use with the script. The lines inside of that
-    file follows the convention used in the reference table for the `Line Number`
-    and `Description` columns.
-
--   Use the script `themes-creator.rb` using the paths for your metadata file and a template from the `templates` directory as arguments. For example: to create a theme for Xresources:
-
+-	Use the script `themes-creator.rb` using the paths for your metadata file and a template from the `templates` directory as arguments. For example: to create a theme for Xresources:
 ```bash
 ruby themes-creator.rb metadata.template templates/xresources.template
 ```
 
 ### Creating A Template
-
-You can create your own templates to use with the script. For that, you must
-write a theme file using the strings described in the `Replaces` column of the
-reference table. They will get replaced by the script by their equivalent data
-from metadata file. Use the `Description` column of the same table to see what
-they mean.
+You can create your own templates to use with the script. For that, you must write a theme file using the strings described in the `Replaces` column of the reference table. They will get replaced by the script by their equivalent data from metadata file. Use the `Description` column of the same table to see what they mean.
 
 ### Testing A Theme
-
-You can preview your terminal emulator's colors by running the
-`tests/preview.rb` script.
-
+You can preview your terminal emulator's colors by running the `tests/preview.rb` script.
 ```bash
 ruby tests/preview.rb
 ```
 
 ## Copyright
-
-This software is under the MIT license. A copy of the license is bundled with
-the source code.
+This software is under the MIT license. A copy of the license is bundled with the source code.
--- themes-creator.rb
+++ themes-creator.rb
@@ -1,120 +1,111 @@
 $PROGRAM_NAME = File.basename(__FILE__, File.extname(__FILE__))
 $PROGRAM_VERSION = "v2.1.2"
 $PROGRAM_HELP =
-  "Usage: #{$PROGRAM_NAME} <METADATA FILE> <TEMPLATE FILE>\n" +
-  "Creates theme files based on templates.\n\n" +
-  "METADATA FLAGS\n" +
-  "The following flags allow you to get information about the program.\n\n" +
-  "  --help     prints these help instructions.\n" +
-  "  --version  prints its version."
+	"Usage: #{$PROGRAM_NAME} <METADATA FILE> <TEMPLATE FILE>\n" +
+	"Creates theme files based on templates.\n\n" +
+	"METADATA FLAGS\n" +
+	"The following flags allow you to get information about the program.\n\n" +
+	"   --help      prints these help instructions.\n" +
+	"   --version   prints its version."
 
-def throw_error(description, suggestion = nil)
-  STDERR.puts("Error Report - #{$PROGRAM_NAME} (1):")
-  STDERR.puts("  Description:")
-  STDERR.puts("    #{description}")
-  if (suggestion)
-    STDERR.puts("\n  Suggestion:")
-    STDERR.puts("    #{suggestion}")
-  end
-  exit(1)
+def Throw_Error(description, suggestion = nil)
+	STDERR.puts("Error Report - #{$PROGRAM_NAME} (1):")
+	STDERR.puts("  Description:")
+	STDERR.puts("    #{description}")
+	if (suggestion)
+		STDERR.puts("\n  Suggestion:")
+		STDERR.puts("    #{suggestion}")
+	end
+	exit(1)
 end
 
-def parse_metadata_flag(flag, text)
-  if (ARGV.include?("--#{flag}"))
-    puts(text)
-    exit(0)
-  end
+def Parse_Metadata_Flag(flag, text)
+	if (ARGV.include?("--#{flag}"))
+		puts(text)
+		exit(0)
+	end
 end
 
-def check_file_existence(name, path)
-  throw_error(
-    "The #{name} file \"#{path}\" does not exists.",
-    "Ensure that you did not misspelled it."
-  ) unless File.exist?(path)
+def Check_File_Existence(name, path)
+	Throw_Error(
+		"The #{name} file \"#{path}\" does not exists.", "Ensure that you did " +
+		"not misspelled it."
+	) unless File.exist?(path)
 end
 
-def parse_colors(colors)
-  parsed_colors = []
-  expected_total_characters = 7
-  colors.each() do |color|
-    if (color[0] != "#")
-      throw_error(
-        "The color \"#{color}\" must start with the \"#\" character.",
-        "Ensure to use it."
-      )
-    elsif (color.length != expected_total_characters)
-      throw_error(
-        "The color \"#{color}\" contains an invalid number of characters.",
-        "Expected #{expected_total_characters} but received #{color.length}."
-      )
-    else
-      color[1..].chars().each() do |character|
-        if (!character.match(/[0-9a-f]/i))
-          throw_error(
-            "The color \"#{color}\" contains an invalid character: " +
-            "\"#{character}\".", "Ensure that you did not " +
-            "misspelled it."
-          )
-        end
-      end
-    end
-    parsed_colors.push(color.downcase())
-  end
-  parsed_colors
+def Parse_Colors(colors)
+	parsed_colors = []
+	expected_total_of_characters = 7
+	colors.each() do |color|
+		Throw_Error(
+			"The color \"#{color}\" must start with the \"#\" character.",
+			"Ensure to use it."
+		) unless color[0] == "#"
+		Throw_Error(
+			"The color \"#{color}\" contains an invalid number of characters.",
+			"Expected #{expected_total_of_characters} but received " +
+			"#{color.length}."
+		) unless color.length == expected_total_of_characters
+		color[1..].chars().each() do |character|
+			Throw_Error(
+				"The color \"#{color}\" contains an invalid character: " +
+				"\"#{character}\".", "Ensure that you did not misspelled it."
+			) unless character.match(/[0-9a-f]/i)
+		end
+		parsed_colors.push(color.downcase())
+	end
+	parsed_colors
 end
 
-def get_metadata()
-  expected_total_lines = 12
-  lines = File.readlines(ARGV[0], chomp: true)
-  if (lines.length != expected_total_lines)
-    throw_error(
-      "The metadata file \"#{ARGV[0]}\" contains an invalid number of lines.",
-      "Expected #{expected_total_lines} but received #{lines.length}."
-    )
-  end
-  return ({
-    name: lines[0].strip(),
-    author: lines[1].strip(),
-    license: lines[2].strip(),
-    url: lines[3].strip(),
-    colors: parse_colors(lines[4..])
-  })
+
+def Get_Metadata()
+	expected_total_of_lines = 12
+	lines = File.readlines(ARGV[0], chomp: true)
+	Throw_Error(
+		"The metadata file \"#{ARGV[0]}\" contains an invalid number of lines.",
+		"Expected #{expected_total_of_lines} but received #{lines.length}."
+	) unless lines.length == expected_total_of_lines
+	return ({
+		name: lines[0].strip(),
+		authors: lines[1].strip(),
+		license: lines[2].strip(),
+		url: lines[3].strip(),
+		colors: Parse_Colors(lines[4..])
+	})
 end
 
-def create_template_string(label)
-  "~%{#{label}}%~"
+def Create_Template_String(label)
+	return ("~%{#{label}}%~")
 end
 
-def apply_template()
-  metadata = get_metadata()
-  template = File.read(ARGV[1])
-  ["name", "author", "license", "url"].each() do |template_string|
-    template.gsub!(
-      create_template_string(template_string),
-      metadata[template_string.to_sym()]
-    )
-  end
-  color_index = 0
-  [
-    "black", "red", "green", "yellow", "blue", "magenta", "cyan", "white"
-  ].each() do |color|
-    template.gsub!(
-      create_template_string(color),
-      metadata[:colors][color_index]
-    )
-    color_index += 1
-  end
-  puts(template)
+def Apply_Template()
+	metadata = Get_Metadata()
+	template = File.read(ARGV[1])
+	["name", "authors", "license", "url"].each() do |template_string|
+		template.gsub!(
+			Create_Template_String(template_string),
+			metadata[template_string.to_sym()]
+		)
+	end
+	color_index = 0
+	[
+		"black", "red", "green", "yellow", "blue", "magenta", "cyan", "white"
+	].each() do |color|
+		template.gsub!(
+			Create_Template_String(color),
+			metadata[:colors][color_index]
+		)
+		color_index += 1
+	end
+	puts(template)
 end
 
-parse_metadata_flag("version", $PROGRAM_VERSION)
-parse_metadata_flag("help", $PROGRAM_HELP)
-if (ARGV.length < 2)
-  throw_error(
-    "Not enough arguments.",
-    "Expected the paths for metadata and theme files."
-  )
-end
-check_file_existence("metadata", ARGV[0])
-check_file_existence("template", ARGV[1])
-apply_template()
+Parse_Metadata_Flag("version", $PROGRAM_VERSION)
+Parse_Metadata_Flag("help", $PROGRAM_HELP)
+Throw_Error(
+	"Not enough arguments.",
+	"Expected the paths for metadata and theme files."
+) unless ARGV.length >= 2
+Check_File_Existence("metadata", ARGV[0])
+Check_File_Existence("template", ARGV[1])
+Apply_Template()
